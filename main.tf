70c40316bc09d8f60bc5d2c47610fc73f2396d7f
uchennaa-ntt <uchenna.anosike@nttdata.com>
2026-05-26 19:59:59 -0500

    Add Azure resources for virtual machine deployment

---
+++ main.tf
@@ -0,0 +1,83 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-gold"
+  address_space       = ["10.10.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-gold"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-gold"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "win2022-gold-build"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_D2s_v3"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter"
+    version   = "latest"
+  }
+}
+
+# Azure Compute Gallery
+resource "azurerm_shared_image_gallery" "gallery" {
+  name                = "goldImageGallery"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+}
+
+resource "azurerm_shared_image" "windows_image" {
+  name                = "Win2022-Gold"
+  gallery_name        = azurerm_shared_image_gallery.gallery.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  os_type             = "Windows"
+
+  identifier {
+    publisher = "Company"
+    offer     = "WindowsServer"
+    sku       = "2022-Gold"
+  }
+}
